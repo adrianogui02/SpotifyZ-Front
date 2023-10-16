71bd61e9814342aa1cc259686373f0aff16aa08c
Adriano Nobre <adrianonobrevg02@gmail.com>
2023-10-16 18:51:13 -0400

    update CustomDatePopup

--- src/components/CustomDatePopup/CustomDatePopup.js
+++ src/components/CustomDatePopup/CustomDatePopup.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./CustomDatePopup.css"; // Importe o arquivo de estilos CSS
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
+import { DateRangeCalendar } from '@mui/x-date-pickers-pro/DateRangeCalendar';
 
 function CustomDatePopup({ inputdatein, inputdateout, onInputChangeIn, onInputChangeOut, onClose }) {
   return (
     <div className="overlay">
       <div className="custom-popup">
-        <input className="input-date" type="text" value={inputdatein} onChange={onInputChangeIn} placeholder="DD-MM-AAAA" />
-        <input className="input-date" type="text" value={inputdateout} onChange={onInputChangeOut} placeholder="DD-MM-AAAA" />
-        <button onClick={onClose}>Buscar</button>
-      </div>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DateRangeCalendar']}>
+        <DateRangeCalendar />
+      </DemoContainer>
+    </LocalizationProvider>
+    <div className="button-container">
+        <button className="button-popup" onClick={onClose}>Buscar</button>
+    </div>
+</div>
+
     </div>
   );
 }
